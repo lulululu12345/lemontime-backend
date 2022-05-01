0fa2c6cb50c05a547485ccfc9b61467c5c57bf7e
Luke Steinmann <luke.steinmann@gmail.com>
2022-05-01 12:16:40 -0400

    uibuild

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/*', (req, res) => {
+app.get('/confirm', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
