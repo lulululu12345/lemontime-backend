afa9d01ac5c1851ab8bf81aaff53f79d0282857b
Luke Steinmann <luke.steinmann@gmail.com>
2022-05-01 11:46:06 -0400

    uibuild

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')))
+
+app.use(express.static(path.join(__dirname, '..', 'build', 'index.html')))
+
 // app.get('/*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 // })
--- controllers/users.js
+++ controllers/users.js
@@ -62,15 +62,14 @@ usersRouter.get('/:confirmationCode', (req, res, next) => {
       }
     })
 
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+    res.status(200)
+    // res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
 
   }).catch((e) => console.log('error', e))
 })
 
-
 // console.log(__dirname)      // "/Users/Sam/dirname-example/src/api"
 // console.log(process.cwd())  // "/Users/Sam/dirname-example"
-
 
 const getTokenFrom = req => {
   const authorization = req.get('authorization')
